6f11a109876a059576b26f793b136629a9e55952
Amber Houle <amber.houle@nordstrom.com>
2018-11-25 13:41:14 -0800

    Add graph problems

---
+++ src/main/java/problems/graphs/GraphColoring.java
@@ -0,0 +1,57 @@
+package problems.graphs;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+/*
+Problems:
+
+Given an undirected graph ↴ with maximum degree ↴ DD, find a graph coloring ↴ using at most D+1D+1 colors.
+
+ */
+
+public class GraphColoring {
+    public static class GraphNode {
+
+        private String label;
+        private Set<GraphNode> neighbors;
+        private Optional<String> color;
+
+        public GraphNode(String label) {
+            this.label = label;
+            neighbors = new HashSet<GraphNode>();
+            color = Optional.empty();
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public Set<GraphNode> getNeighbors() {
+            return Collections.unmodifiableSet(neighbors);
+        }
+
+        public void addNeighbor(GraphNode neighbor) {
+            neighbors.add(neighbor);
+        }
+
+        public boolean hasColor() {
+            return color.isPresent();
+        }
+
+        public String getColor() {
+            return color.get();
+        }
+
+        public void setColor(String color) {
+            this.color = Optional.ofNullable(color);
+        }
+    }
+
+    public static void colorGraph(GraphNode[] graph, String[] colors) {
+        // create a valid coloring for the graph
+    }
+
+}
